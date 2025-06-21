5637c6ac8a1c1a5bb200b1fcfcdc7bb6a8442324
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2025-06-21 12:58:25 -0400

    Added more comments

--- decoder.py
+++ decoder.py
@@ -4,11 +4,11 @@ def int24_be(b: bytes):
     return val - 0x1000000 if val & 0x800000 else val
 
 
-def decode_coords(hex_line: str):
+def decode_coords(hex_str: str):
     # strip spaces, convert to bytes
-    pkt = bytes.fromhex(hex_line.replace(" ", ""))
+    pkt = bytes.fromhex(hex_str.replace(" ", ""))
     # find the 0x25 marker (it’s always there)
     i = pkt.index(0x25) + 1
-    lat_raw = int24_be(pkt[i : i + 3])
-    lon_raw = int24_be(pkt[i + 3 : i + 6])
+    lat_raw = int24_be(pkt[i : i + 3])  # 3 bytes
+    lon_raw = int24_be(pkt[i + 3 : i + 6])  # 3 bytes
     return lat_raw / 30000, lon_raw / 30000
